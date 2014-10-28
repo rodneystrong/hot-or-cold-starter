1e56e41fa9f13a2c1ec56e5c652a33a465ac076c
aleeex <akang2@gmail.com>
2014-10-28 15:07:28 -0700

    fixed issue wtih userGuess always being 0. Put the userGuess variable inside the feedback event function

--- js/app.js
+++ js/app.js
@@ -12,8 +12,6 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-  	//the user guess
-  	var userGuess = +$('#userGuess').val();
   	//the secret number
   	var secretNum = Math.floor(Math.random() * (100 - 1));;
 
@@ -43,40 +41,40 @@ $(document).ready(function(){
   	}
 
   	/*feedback to users*/
-  	function guessFeedback() {
-  		$('#guessButton').on('click', function(e) {
-  			//prevent the default submit behavior
-  			e.preventDefault()
-  			//unit test
-  			//console.log(secretNum);
-  			if(userGuess < 1 || userGuess > 100 || userGuess % 1 !== 0) {
-  				$('#feedback').text('Please enter a whole # between 1 and 100');
-  			}
-  			if(userGuess < secretNum) {
-  				//console.log('too low');
-  				$('#feedback').text('too low');
-  				return;
-  			}
-  			else if(userGuess > secretNum) {
-  				$('#feedback').text('too high');
-  				return;
-  			}
-  			else {
-  				$('#feedback').text('correct!');
-  			}
-  		});
-  	}
+  	$('#guessButton').on('click', function(e) {
+  		//prevent the default submit behavior
+  		e.preventDefault()
+  		//the user guess
+  		var userGuess = +$('#userGuess').val();
+  		console.log(userGuess);
+  		//unit test
+  		//console.log(secretNum);
+  		if(userGuess < 1 || userGuess > 100 || userGuess % 1 !== 0) {
+  			$('#feedback').text('Please enter a whole # between 1 and 100');
+  		}
+  		else if(userGuess < secretNum) {
+  			//console.log('too low');
+  			$('#feedback').text('too low');
+  			return;
+  		}
+  		else if(userGuess > secretNum) {
+  			$('#feedback').text('too high');
+  			return;
+  		}
+  		else {
+  			$('#feedback').text('correct!');
+  		}
+  	});
 
-  		$('#guessButton').on('click', function() {
-  			guessCount += 1;
-  			//unit test
-  			//console.log(guessCount);
-  			$('#count').text(guessCount);
-  			$('#userGuess').focus();
-  		});
+  	$('#guessButton').on('click', function() {
+  		guessCount += 1;
+  		//unit test
+  		//console.log(guessCount);
+  		$('#count').text(guessCount);
+  		$('#userGuess').focus();
+  	});
 
   	newGame();
-  	guessFeedback();
   	/*feedback to user*/
   	//var to take user input
   	//console.log(userGuess);
